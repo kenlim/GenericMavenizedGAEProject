a8e8072ec2dc6ee2ec9ec0d383ee93d8dd70546a
Ken Lim <kenneth.lim@guardian.co.uk>
2010-05-28 14:29:21 +0100

    Removed invalid import

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,9 +1,7 @@
-import namespace.models.ElGenerico;
 import org.junit.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 public class SimpleTest {
     @Test
